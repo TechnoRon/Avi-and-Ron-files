cdada3857e243732062ba57fcfac65c6af10ed0d
Wolfe359 <aviwburstein@gmail.com>
2018-09-30 15:38:45 -0500

    Update random number generator.py

--- random number generator.py
+++ random number generator.py
@@ -8,7 +8,7 @@ if abs(numberchosen - person)< abs(numberchosen - computer):
     print("You win")
 elif abs(numberchosen - person)> abs(numberchosen - computer):
     print("You lose")
-numberchoseninfo = "The number was {}".format(numberchosen)
-computernumberinfo = "The computer chose {}".format(computer)
+numberchoseninfo = "The number was " + str(numberchosen)
+computernumberinfo = "The computer chose " + str(computer)
 print(numberchoseninfo)
 print(computernumberinfo)
