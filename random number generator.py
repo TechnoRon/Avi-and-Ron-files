62020fe7fe5b1e007a0dfbdc5009683454c51b3f
TechnoRon <41198544+TechnoRon@users.noreply.github.com>
2018-09-30 15:22:23 -0500

    Add files via upload

---
+++ random number generator.py
@@ -0,0 +1,14 @@
+import random
+computer = random.randint(1,10)
+numberchosen = random.randint(1,10)
+print("Welcome to the number game!")
+print("The game is simple - both you and the computer choose a number, and whoever is closer to the designated number wins!")
+person = int(input("Choose a random number from 1 to 10 inclusively. "))
+if abs(numberchosen - person)< abs(numberchosen - computer):
+    print("You win")
+elif abs(numberchosen - person)> abs(numberchosen - computer):
+    print("You lose")
+numberchoseninfo = "The number was {}".format(numberchosen)
+computernumberinfo = "The computer chose {}".format(computer)
+print(numberchoseninfo)
+print(computernumberinfo)
